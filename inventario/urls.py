ef9506015e555f6f7dc6ff188c68d04f0ed14b29
fabstao <fabian@nuo.com.mx>
2019-11-29 13:40:20 -0600

    Demo 1

--- inventario/urls.py
+++ inventario/urls.py
@@ -18,6 +18,7 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('relacion.urls')),
     path('relacion/', include('relacion.urls')),
     path('administracion/', include('administracion.urls')),
 ]
